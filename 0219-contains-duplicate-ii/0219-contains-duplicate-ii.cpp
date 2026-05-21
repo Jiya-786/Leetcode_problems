6f7e75e76d42430ff220f8730fac93ac739f61a7
Jiya_07 <merjajiya@gmail.com>
2026-05-21 14:36:30 +0530

    Time: 75 ms (59.61%), Space: 98.8 MB (13.63%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,14 @@
+// using hashmap to store latest index becuase if elt is at position 2,7,15 nad we are checking at position 17 with k=3 and 15 does not satisfy, defi 2 nad 7 will also not satisfy
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int> lastSeen;
+        for(int i=0;i<nums.size();i++){
+            if(lastSeen.count(nums[i]) && (i-lastSeen[nums[i]]<=k)){
+                return true;
+            }
+            lastSeen[nums[i]]=i;
+        }
+        return false;
+    }
+};
