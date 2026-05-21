cb0f5a37f34816a8389113253ac41f5a6a10e4ce
Jiya_07 <merjajiya@gmail.com>
2026-05-21 16:29:15 +0530

    Time: 51 ms (16.81%), Space: 29.7 MB (19.41%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,12 +1,22 @@
+// using freq as key using delimiter
+// avoids sorting entirely and brings per-string cost down from o(klogk) to o(k)
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>> groups;
 
         for(const string& s:strs){
-            string key=s;
-            sort(key.begin(),key.end());
+            int freq[26]={0};
+            for(char c:s){
+                freq[c-'a']++;
+            }
 
+            // build a unique key from freq array
+            string key;
+            for(int i=0;i<26;i++){
+                key+='#';
+                key+=to_string(freq[i]);
+            }
             groups[key].push_back(s);
         }
 
