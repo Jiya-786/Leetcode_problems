16757469a6fa4e96e2264486e1cf5ad986295e43
Jiya_07 <merjajiya@gmail.com>
2026-05-21 17:41:33 +0530

    Time: 4 ms (55.29%), Space: 10.5 MB (32.16%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
@@ -1,5 +1,10 @@
 // time o(1)
 // space o(n)
+// this is the most basic version
+// it gives duplicate short url for same long url
+// since we are not checking if we have shortened the url already
+// this can be improved using randomizing the counter aka key and maintaing a hash to chek if we 
+// have already shortened the url
 class Solution {
     unordered_map<int,string> map;
     int counter=0;
