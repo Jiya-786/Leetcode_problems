ab00429cdf2ab7ed6a5249e510f5c06791ac5d16
Jiya_07 <merjajiya@gmail.com>
2026-05-21 14:37:27 +0530

    Time: 71 ms (77.76%), Space: 98.8 MB (13.63%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,4 +1,6 @@
 // using hashmap to store latest index becuase if elt is at position 2,7,15 nad we are checking at position 17 with k=3 and 15 does not satisfy, defi 2 nad 7 will also not satisfy
+// time o(n)
+// space o(n)
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
