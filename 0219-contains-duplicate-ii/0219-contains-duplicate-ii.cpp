12322cc43b34aa1111790139bbfe38a62494b5e8
Jiya_07 <merjajiya@gmail.com>
2026-05-21 14:53:35 +0530

    Time: 80 ms (36.22%), Space: 93.8 MB (66.02%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,15 +1,19 @@
-// using hashmap to store latest index becuase if elt is at position 2,7,15 nad we are checking at position 17 with k=3 and 15 does not satisfy, defi 2 nad 7 will also not satisfy
+// using sliding window 
 // time o(n)
-// space o(n)
+// space o(min(n,k))
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int,int> lastSeen;
+        unordered_set<int> window;
         for(int i=0;i<nums.size();i++){
-            if(lastSeen.count(nums[i]) && (i-lastSeen[nums[i]]<=k)){
+            if(window.count(nums[i])){
                 return true;
             }
-            lastSeen[nums[i]]=i;
+            window.insert(nums[i]);
+
+            if(window.size()>k){
+                window.erase(nums[i-k]);
+            }
         }
         return false;
     }
