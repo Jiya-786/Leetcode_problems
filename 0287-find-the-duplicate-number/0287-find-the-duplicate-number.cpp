684c8dc0647c9b3e33741b1edbe35933894aecd5
Jiya_07 <merjajiya@gmail.com>
2026-06-12 01:14:34 +0530

    Time: 15 ms (38.76%), Space: 65.2 MB (40.48%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,3 +1,8 @@
+// USING CYCLE SORT
+// similar to set-mismatch logic
+// qhenever we find a mismatch, i+1 will be the missing number and nums[i] will be the duplicate
+// becuase duplicates in cycle sort get stuck in the worng postion
+// and this qs guarantees only 1 duplicate, hence it is easier to solve
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
