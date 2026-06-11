b8288af9b4b153b19fa66d6348959414d6c49e7d
Jiya_07 <merjajiya@gmail.com>
2026-06-11 23:51:32 +0530

    Time: 0 ms (100%), Space: 55.2 MB (29.72%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,27 +1,35 @@
-// using sorting
-// time o(nlogn)
-// space o(1)
-// this time-space tradeoff is ipp to the one in hashset, none are optimal but it reveals the problem beautifully
+// we can use the input array itself as a hashmap
+// the answer is always in the range [1,n+1]
+// we want to check which numbers from 1 to n are present and which are not
+// value i at index i-1
+// then first index where mismatch happens is the ans 
+// this is cycle sort
+// for each position we keep swapping the current elt to its correct position until- 
+            //   either the current elt is out of range(-ve,0, or >n)
+            //   current elt is already in correct position
+            //   target position already has correct value(duplicate)
+// after this arrangement, one pass through the array gives us the ans
 
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        
-        int expected=1;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]<=0 || i>0 && nums[i]==nums[i-1]){
-                continue;
-            }
-            else{
-                if(nums[i]==expected){
-                    expected++;
-                }
-                else{
-                    break;
-                }
+        int n=nums.size();
+
+        // place each value i at index i-1 using cycle sort
+        for(int i=0;i<n;i++){
+            while(nums[i]>=1 && nums[i]<=n && nums[nums[i]-1]!=nums[i]){     // this handles
+                                                                             // <=0, >n 
+                                                                             // duplicates
+                swap(nums[i],nums[nums[i]-1]);
             }
         }
-        return expected;
+
+        // find the first position where the value does not match
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1){
+                return i+1;
+            }
+        }
+        return n+1;
     }
 };
