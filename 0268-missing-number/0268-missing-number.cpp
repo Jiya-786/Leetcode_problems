e95f35a0d129ccac4c79650005929cf507bac95b
Jiya_07 <merjajiya@gmail.com>
2026-06-12 00:12:05 +0530

    Time: 2 ms (20.31%), Space: 21.6 MB (99.79%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        
+        int i=0;
+
+        while(i<n){
+            if(nums[i]<n && nums[i]!=nums[nums[i]]){   // if no is in range and still not in
+                                                       // correct position, bring it into
+                                                       // correct position
+                swap(nums[i],nums[nums[i]]);
+            }
+            else{
+                i++;  // if no. not in range, move on to next number
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i){
+                return i;
+            }
+        }
+        return n;
+    }
+};
