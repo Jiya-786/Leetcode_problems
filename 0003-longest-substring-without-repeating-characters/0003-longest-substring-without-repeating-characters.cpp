4e484261e91fd414cce38d53da507c5ac415159b
Jiya_07 <merjajiya@gmail.com>
2026-06-16 11:30:58 +0530

    Time: 15 ms (37.48%), Space: 14.4 MB (12.55%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,65 @@
+// this code counts unique characters we need longest substring from the string
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         unordered_map<char,int> map;
+//         for(char c:s){
+//             map[c]++;
+//         }
+//         int count=0;
+//         for(auto& it:map){
+//             if(it.second()){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+// BRUTE FORCE
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         int maxLen=0;
+//         int n=s.size();
+
+//         for(int i=0;i<n;i++){
+//             unordered_set<char> seen;
+//             for(int j=i;j<n;j++){
+//                 if(seen.count(s[j])) break;
+//                 seen.insert(s[j]);
+//                 maxLen=max(maxLen,j-i+1);
+//             }
+//         }
+//         return maxLen;
+//     }
+// };
+
+// we will use sliding window technique
+
+// we move the right pointer to add a new character to the window
+
+// if the new character is already in the window, move left pointer form=ward until the duplicate is gone
+
+// at each step caluclate window size using right-left+1 and keep track of the maximum
+// we use an array of size 256 to store the last seen index of each character. this allows left pointer to jump instead of sliding by one in case of sliding window max
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n=s.size();
+
+        unordered_set<char>window;
+        int left=0;
+        int maxLen=0;
+
+        for(int i=0;i<n;i++){
+            while(window.count(s[i])){
+                window.erase(s[left]);
+                left++;
+            }
+            window.insert(s[i]);
+            maxLen=max(maxLen,i-left+1);
+        }
+        return maxLen;
+    }
+};
