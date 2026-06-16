242f550a59df60e1cc42c5d91a86d517f7a28737
Jiya_07 <merjajiya@gmail.com>
2026-06-16 12:06:58 +0530

    Time: 7 ms (63.89%), Space: 11.7 MB (76.65%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,45 +1,19 @@
-// BRUTE FORCE
-// class Solution {
-// public:
-//     int lengthOfLongestSubstring(string s) {
-//         int maxLen=0;
-//         int n=s.size();
+// Instead of a set that only tells us whether a character is in the window, we use a hash map that stores the most recent index of each character. When we encounter a duplicate, we can jump the left pointer directly past the previous occurrence, no need to slide one step at a time.
 
-//         for(int i=0;i<n;i++){
-//             unordered_set<char> seen;
-//             for(int j=i;j<n;j++){
-//                 if(seen.count(s[j])) break;
-//                 seen.insert(s[j]);
-//                 maxLen=max(maxLen,j-i+1);
-//             }
-//         }
-//         return maxLen;
-//     }
-// };
-
-// we will use sliding window technique
-
-// we move the right pointer to add a new character to the window
-
-// if the new character is already in the window, move left pointer form=ward until the duplicate is gone
-
-// at each step caluclate window size using right-left+1 and keep track of the maximum
-// we use an array of size 256 to store the last seen index of each character. this allows left pointer to jump instead of sliding by one in case of sliding window max
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n=s.size();
-
-        unordered_set<char>window;
+        unordered_map<char,int> lastIndex;
         int left=0;
         int maxLen=0;
 
         for(int i=0;i<n;i++){
-            while(window.count(s[i])){
-                window.erase(s[left]);
-                left++;
+            char c=s[i];
+            if(lastIndex.count(c) && lastIndex[c]>=left){
+                left=lastIndex[c]+1;
             }
-            window.insert(s[i]);
+            lastIndex[c]=i;
             maxLen=max(maxLen,i-left+1);
         }
         return maxLen;
