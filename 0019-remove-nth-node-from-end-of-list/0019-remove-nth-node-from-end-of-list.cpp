d93f2ae5445d3fbded8a2a3d13640970581a4a65
Jiya_07 <merjajiya@gmail.com>
2026-06-16 16:59:10 +0530

    Time: 0 ms (100%), Space: 14.9 MB (92.67%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+ // using the dummy node in place of head
+ // otherwise, it is like head -> 1 -> 2 -> 3 -> 4 -> 5
+ // dummy replaces head with a node that has value 0 and points to the same thing as the head
+ // now: dummy -> 1 -> 2 -> 3 -> 4 -> 5
+ //               ^
+ //               head
+ // now it is easy to remove first node ie 1 and return pointer(dummy) again
+ // dummy trick is one of the most used in LL qs
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode dummy(0,head);
+
+        int length=0;
+        ListNode* current=head;
+        while(current!=NULL){
+            length++;
+            current=current->next;
+        }
+
+        ListNode* prev= &dummy;
+// so now it will look like prev -> dummy -> 1 -> 2 -> 3 -> 4 -> 5
+//                                           ^
+//                                           head
+// this setup allows us to delete head ie 1 the same way we would delete other nodes
+        for(int i=0;i<length-n;i++){
+            prev=prev->next;
+        }
+        ListNode* toDelete=prev->next;
+        prev->next=prev->next->next;
+        delete toDelete;
+
+        return dummy.next;
+    }
+};
