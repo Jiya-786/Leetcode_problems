2d2db674b55de2ad444aa437f8169ec3a6dbd179
Jiya_07 <merjajiya@gmail.com>
2026-06-17 10:58:11 +0530

    Time: 43 ms (28.56%), Space: 152.3 MB (6.68%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,14 +1,52 @@
+// // class MinStack {
+// //     vector<int> stack;   // because vector as a stack allows random access
+// // public:
+// //     MinStack() {}
+    
+// //     void push(int val) {
+// //         stack.push_back(val);        
+// //     }
+    
+// //     void pop() {
+// //         stack.pop_back();
+// //     }
+    
+// //     int top() {
+// //         return stack.back();   // push_back(10); push_back(20); push_back(30); vector: [10,20,30] 
+// //                         // the last elt is the most recently added, and hence at the top
+// //     }
+    
+// //     // BRUTE FORCE
+// //     // getMin is not o(1)
+// //     int getMin() {
+// //         int minVal=stack[0];
+// //         for(int a:stack){
+// //             minVal=min(a,minVal);
+// //         }
+// //         return minVal;
+// //     }
+// // };
+
 // class MinStack {
 //     vector<int> stack;   // because vector as a stack allows random access
+//     vector<int> minStack;
+
 // public:
 //     MinStack() {}
     
 //     void push(int val) {
-//         stack.push_back(val);        
+//         stack.push_back(val);
+//         if(minStack.empty()){
+//             minStack.push_back(val);
+//         }
+//         else{
+//             minStack.push_back(min(val,minStack.back()));
+//         }        
 //     }
     
 //     void pop() {
 //         stack.pop_back();
+//         minStack.pop_back();
 //     }
     
 //     int top() {
@@ -16,45 +54,30 @@
 //                         // the last elt is the most recently added, and hence at the top
 //     }
     
-//     // BRUTE FORCE
-//     // getMin is not o(1)
 //     int getMin() {
-//         int minVal=stack[0];
-//         for(int a:stack){
-//             minVal=min(a,minVal);
-//         }
-//         return minVal;
+//         return minStack.back();
 //     }
 // };
 
 class MinStack {
-    vector<int> stack;   // because vector as a stack allows random access
-    vector<int> minStack;
-
+    vector<pair<int,int>> stack;
 public:
     MinStack() {}
     
     void push(int val) {
-        stack.push_back(val);
-        if(minStack.empty()){
-            minStack.push_back(val);
-        }
-        else{
-            minStack.push_back(min(val,minStack.back()));
-        }        
+        int req= stack.empty() ? val : min(val,stack.back().second);
+        stack.push_back({val,req});
     }
     
     void pop() {
         stack.pop_back();
-        minStack.pop_back();
     }
     
     int top() {
-        return stack.back();   // push_back(10); push_back(20); push_back(30); vector: [10,20,30] 
-                        // the last elt is the most recently added, and hence at the top
+        return stack.back().first;
     }
     
     int getMin() {
-        return minStack.back();
+        return stack.back().second;
     }
 };
