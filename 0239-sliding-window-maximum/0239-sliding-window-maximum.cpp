8f13246855b79622df10c455e40d3dd5c233f03c
Jiya_07 <merjajiya@gmail.com>
2026-06-17 15:34:12 +0530

    Time: 66 ms (14.64%), Space: 153.3 MB (11.42%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,44 @@
+// // BRUTE FORCE
+// class Solution {
+// public:
+//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+//         int n=nums.size();
+//         vector<int> ans;
+
+//         for(int i=0;i<=n-k;i++){
+//             int req=nums[i];
+//             for(int j=i+1;j<i+k;j++){
+//                 req=max(req,nums[j]);
+//             }
+//             ans.push_back(req);
+//         }
+//         return ans;
+//     }   
+// };
+
+// PRIORITY QUEUE METHOD
+// time o(nlogn)
+// space o(n)
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int> ans;
+        priority_queue<pair<int,int>> pq;
+
+        for(int i=0;i<k;i++){
+            pq.push({nums[i],i});
+        }
+        ans.push_back(pq.top().first);
+
+        for(int i=k;i<n;i++){
+            pq.push({nums[i],i});
+            // removing elts outside the window
+            while(pq.top().second<=i-k){
+                pq.pop();
+            }
+            ans.push_back(pq.top().first);
+        }
+        return ans;
+    }
+};
