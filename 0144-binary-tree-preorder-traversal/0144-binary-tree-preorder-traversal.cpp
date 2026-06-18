ffcd128dfc34510b810dd96fae22e3cbbdb0eba7
Jiya_07 <merjajiya@gmail.com>
2026-06-18 19:07:01 +0530

    Time: 0 ms (100%), Space: 11 MB (42.19%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -1,26 +1,52 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right; *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right; *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+
+// RECURSIVE SOLUTION
+// class Solution {
+// public:
+//     vector<int> preorderTraversal(TreeNode* root) {
+//         vector<int> result;
+//         preorder(root,result);
+//         return result;
+//     }
+// private:
+//     void preorder(TreeNode* node,vector<int>& result){
+//         if(node==nullptr) return;
+
+//         result.push_back(node->val);
+//         preorder(node->left,result);
+//         preorder(node->right,result);
+//     }
+// };
+
+// ITERATIVE SOLUTION USING STACK
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> result;
-        preorder(root,result);
-        return result;
-    }
-private:
-    void preorder(TreeNode* node,vector<int>& result){
-        if(node==nullptr) return;
+        stack<TreeNode*> s;
 
-        result.push_back(node->val);
-        preorder(node->left,result);
-        preorder(node->right,result);
+        if(root==nullptr) return result;
+        s.push(root);
+
+        while(!s.empty()){
+            TreeNode* node=s.top();
+            result.push_back(node->val);
+            s.pop();
+
+            if(node->right) s.push(node->right);
+            if(node->left) s.push(node->left);
+        }
+
+        return result;
+        
     }
 };
