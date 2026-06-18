494f9f08eb00e084e16781e499e33a9baf8fe9c8
Jiya_07 <merjajiya@gmail.com>
2026-06-18 14:32:09 +0530

    Time: 0 ms (100%), Space: 17.2 MB (15.47%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -1,36 +1,65 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     vector<vector<int>> levelOrder(TreeNode* root) {
+//         vector<vector<int>> result;
+//         dfs(root,0,result);
+//         return result;
+//     }
+// private:
+//     void dfs(TreeNode* node, int level, vector<vector<int>>& result){
+//         if(node==nullptr) return;
+
+//         // if first time at this level
+//         if(level==result.size()){
+//             result.push_back({});
+//         }
+
+//         // add current node's value to this level
+//         result[level].push_back(node->val);
+
+//         // recurse left to maintain left to right order
+//         dfs(node->left,level+1,result);
+//         dfs(node->right,level+1,result);
+
+//     }
+// };
+
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>> result;
-        dfs(root,0,result);
-        return result;
-    }
-private:
-    void dfs(TreeNode* node, int level, vector<vector<int>>& result){
-        if(node==nullptr) return;
+        if(root==nullptr) return result;
 
-        // if first time at this level
-        if(level==result.size()){
-            result.push_back({});
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            int levelSize=q.size();
+            vector<int> currentLevel;
+
+            for(int i=0;i<levelSize;i++){
+                TreeNode* node=q.front();
+                q.pop();
+                currentLevel.push_back(node->val);
+
+                // enqueue children for next level
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+
+            result.push_back(currentLevel);
         }
-
-        // add current node's value to this level
-        result[level].push_back(node->val);
-
-        // recurse left to maintain left to right order
-        dfs(node->left,level+1,result);
-        dfs(node->right,level+1,result);
-
+        return result;
     }
 };
