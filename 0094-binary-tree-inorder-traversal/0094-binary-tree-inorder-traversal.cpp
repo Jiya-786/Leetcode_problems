e29d9665390a75e3f4c036f6b2e9de936e6e91f7
Jiya_07 <merjajiya@gmail.com>
2026-06-19 00:04:23 +0530

    Time: 0 ms (100%), Space: 10.9 MB (65.84%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,39 +1,72 @@
-// // RECURSIVE
+// // // RECURSIVE
+// // class Solution {
+// // public:
+// //     vector<int> inorderTraversal(TreeNode* root) {
+// //         vector<int> result;
+// //         inorder(root,result);
+// //         return result;
+// //     }
+// // private:
+// //     void inorder(TreeNode* node,vector<int>& result){
+// //         if(node==nullptr) return;
+
+// //         inorder(node->left,result);
+// //         result.push_back(node->val);
+// //         inorder(node->right,result);
+// //     }
+// // };
+
+// // ITERATIVE
 // class Solution {
 // public:
 //     vector<int> inorderTraversal(TreeNode* root) {
+//         stack<TreeNode*> st;
 //         vector<int> result;
-//         inorder(root,result);
-//         return result;
-//     }
-// private:
-//     void inorder(TreeNode* node,vector<int>& result){
-//         if(node==nullptr) return;
+//         TreeNode* curr=root;
 
-//         inorder(node->left,result);
-//         result.push_back(node->val);
-//         inorder(node->right,result);
+//         while(curr||!st.empty()){
+//             while(curr){
+//                 st.push(curr);
+//                 curr=curr->left;
+//             }
+//             curr=st.top();
+//             st.pop();
+//             result.push_back(curr->val);
+
+//             curr=curr->right;
+//         }
+//         return result;
 //     }
 // };
 
-// ITERATIVE
+// MORRIS TRAVERSAL
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        stack<TreeNode*> st;
         vector<int> result;
         TreeNode* curr=root;
 
-        while(curr||!st.empty()){
-            while(curr){
-                st.push(curr);
-                curr=curr->left;
+        while(curr!=nullptr){
+            if(curr->left==nullptr){
+                result.push_back(curr->val);
+                curr=curr->right;
             }
-            curr=st.top();
-            st.pop();
-            result.push_back(curr->val);
-
-            curr=curr->right;
+            else{
+                TreeNode* req=curr->left;
+                while(req->right!=nullptr && req->right!=curr){
+                    req=req->right;
+                }
+                if(req->right==nullptr){  // this would be our first visit
+                    req->right=curr;   // create thread
+                    
+                    curr=curr->left;
+                }
+                else{         // this would be our 2nd visit
+                    req->right=nullptr;   // break the thread;
+                    result.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
         }
         return result;
     }
