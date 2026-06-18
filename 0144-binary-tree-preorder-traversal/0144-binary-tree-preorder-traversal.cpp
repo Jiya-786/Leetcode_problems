d95037fe813603e6db49f2f1e3888ebb431237e6
Jiya_07 <merjajiya@gmail.com>
2026-06-18 23:39:43 +0530

    Time: 0 ms (100%), Space: 10.8 MB (73.18%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -27,26 +27,69 @@
 //     }
 // };
 
-// ITERATIVE SOLUTION USING STACK
+// // ITERATIVE SOLUTION USING STACK
+// class Solution {
+// public:
+//     vector<int> preorderTraversal(TreeNode* root) {
+//         vector<int> result;
+//         stack<TreeNode*> s;
+
+//         if(root==nullptr) return result;
+//         s.push(root);
+
+//         while(!s.empty()){
+//             TreeNode* node=s.top();
+//             result.push_back(node->val);
+//             s.pop();
+
+//             if(node->right) s.push(node->right);
+//             if(node->left) s.push(node->left);
+//         }
+
+//         return result;
+        
+//     }
+// };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> result;
-        stack<TreeNode*> s;
+        TreeNode* curr=root;
 
-        if(root==nullptr) return result;
-        s.push(root);
-
-        while(!s.empty()){
-            TreeNode* node=s.top();
-            result.push_back(node->val);
-            s.pop();
-
-            if(node->right) s.push(node->right);
-            if(node->left) s.push(node->left);
+        while(curr!=nullptr){
+            if(curr->left==nullptr){
+                result.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{
+                TreeNode* req=curr->left;
+                while(req->right!=nullptr && req->right!=curr){
+                    req=req->right;
+                }
+                if(req->right==nullptr){  // this would be our first visit
+                    req->right=curr;   // create thread
+                    result.push_back(curr->val);
+                    curr=curr->left;
+                }
+                else{         // this would be our 2nd visit
+                    req->right=nullptr;   // break the thread;
+                    curr=curr->right;
+                }
+            }
         }
-
         return result;
-        
     }
 };
+
