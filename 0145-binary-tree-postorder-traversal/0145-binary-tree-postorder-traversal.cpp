e1ab20a0bb19c6826d8ae069964cb95dab7d7d7c
Jiya_07 <merjajiya@gmail.com>
2026-06-19 09:09:56 +0530

    Time: 0 ms (100%), Space: 10.9 MB (72.85%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -1,40 +1,51 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
 
-// RECURSIVE SOLUTION
+// // RECURSIVE SOLUTION
+// class Solution {
+// public:
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int> result;
+//         postorder(root,result);
+//         return result;
+//     }
+// private:
+//     void postorder(TreeNode* node,vector<int>& result){
+//         if(node==nullptr) return;
+
+//         if(node->left) postorder(node->left,result);
+//         if(node->right) postorder(node->right,result);
+//         result.push_back(node->val);
+//     }
+// };
+
+//  ITERATIVE SOLUTION
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> result;
-        postorder(root,result);
-        return result;
-    }
-private:
-    void postorder(TreeNode* node,vector<int>& result){
-        if(node==nullptr) return;
+        stack<TreeNode*> st;
 
-        if(node->left) postorder(node->left,result);
-        if(node->right) postorder(node->right,result);
-        result.push_back(node->val);
+        if(root==nullptr) return result;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* node=st.top();
+            st.pop();
+            result.push_back(node->val);
+
+            if(node->left) st.push(node->left);
+            if(node->right) st.push(node->right);
+        }
+        reverse(result.begin(),result.end());
+        return result;
     }
 };
