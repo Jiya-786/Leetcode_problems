7d36af781733c7e2cc86dcb34d547317675cf9dc
Jiya_07 <merjajiya@gmail.com>
2026-06-20 16:56:09 +0530

    Time: 0 ms (100%), Space: 23.6 MB (95.43%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
@@ -1,39 +1,71 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
 
-// ITERATIVE BFS
+// // ITERATIVE BFS
+// class Solution {
+// public:
+//     vector<double> averageOfLevels(TreeNode* root) {
+//          queue<TreeNode*> q;
+//          q.push(root);          // given min 1 node exists hence if(root==nullptr) is not req
 
+//          vector<double> result;
+
+//          while(!q.empty()){
+//             int n=q.size();
+//             int long long sum=0;
+//             for(int i=0;i<n;i++){
+//                 TreeNode* node=q.front();
+//                 q.pop();
+//                 sum+=node->val;
+
+//                 if(node->left) q.push(node->left);
+//                 if(node->right) q.push(node->right);
+//             }
+//             result.push_back((double)sum/n);
+//          }
+//          return result;
+
+//     }
+// };
+
+// DFS METHOD
+// it might look like level order/bfs but its dfs since we do root->left->right
 class Solution {
 public:
     vector<double> averageOfLevels(TreeNode* root) {
-         queue<TreeNode*> q;
-         q.push(root);          // given min 1 node exists hence if(root==nullptr) is not req
+        vector<long long> sums;
+        vector<int> counts;
 
-         vector<double> result;
+        dfs(root,0,sums,counts);
 
-         while(!q.empty()){
-            int n=q.size();
-            int long long sum=0;
-            for(int i=0;i<n;i++){
-                TreeNode* node=q.front();
-                q.pop();
-                sum+=node->val;
+        vector<double> result;
+        for(int i=0;i<counts.size();i++){
+            result.push_back((double)sums[i]/counts[i]);
+        }
 
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
-            }
-            result.push_back((double)sum/n);
-         }
-         return result;
+        return result;
 
+    }
+private:
+    void dfs(TreeNode* node,int depth,vector<long long>& sums,vector<int>& counts){
+        if(depth==sums.size()){
+            sums.push_back(node->val);
+            counts.push_back(1);
+        }
+        else{
+            sums[depth]+=node->val;
+            counts[depth]++;
+        }
+        if(node->left) dfs(node->left,depth+1,sums,counts);
+        if(node->right) dfs(node->right,depth+1,sums,counts);
     }
 };
