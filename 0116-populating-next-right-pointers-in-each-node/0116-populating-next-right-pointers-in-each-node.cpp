1c9ea0202c4fb6f394b5d78ba95159e61fdfbe47
Jiya_07 <merjajiya@gmail.com>
2026-06-21 17:08:29 +0530

    Time: 7 ms (98%), Space: 19 MB (66.1%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -1,43 +1,73 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* left;
-    Node* right;
-    Node* next;
+// /*
+// // Definition for a Node.
+// class Node {
+// public:
+//     int val;
+//     Node* left;
+//     Node* right;
+//     Node* next;
 
-    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+//     Node() : val(0), left(NULL), right(NULL), next(NULL) {}
 
-    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+//     Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
 
-    Node(int _val, Node* _left, Node* _right, Node* _next)
-        : val(_val), left(_left), right(_right), next(_next) {}
-};
-*/
+//     Node(int _val, Node* _left, Node* _right, Node* _next)
+//         : val(_val), left(_left), right(_right), next(_next) {}
+// };
+// */
 
-// USING BFS
+// // USING BFS
+// this is o(n) space soln, qs requires o(1) space solution
+// class Solution {
+// public:
+//     Node* connect(Node* root) {
+//         queue<Node*> q;
+//         if(root==nullptr) return root;
+//         q.push(root);
+
+//         while(!q.empty()){
+//             int n=q.size();
+//             for(int i=0;i<n;i++){
+//                 Node* node1=q.front();
+//                 q.pop();
+//                 if(i==n-1){
+//                      node1->next=nullptr;
+//                 }
+//                 else{
+//                     node1->next=q.front();
+//                 }
+//                 if(node1->left) q.push(node1->left);
+//                 if(node1->right) q.push(node1->right);
+//             }
+//         }
+//         return root;
+//     }
+// };
+
+// now doing it in constant o(1) space
+// in the qs we are given left and right child pointer for each node
+// and also given that the tree is a perfect binary tree
+// implies each node has both left and right children
 class Solution {
 public:
     Node* connect(Node* root) {
-        queue<Node*> q;
-        if(root==nullptr) return root;
-        q.push(root);
+        Node* lvlStart=root;
+        if(!root) return root;
 
-        while(!q.empty()){
-            int n=q.size();
-            for(int i=0;i<n;i++){
-                Node* node1=q.front();
-                q.pop();
-                if(i==n-1){
-                     node1->next=nullptr;
+        while(lvlStart->left){
+            Node* node=lvlStart;
+
+            while(node){
+                node->left->next=node->right;   // it's a perfect binary tree, hence left and right child 
+                                                // always exist except in case of leaf nodes
+
+                if(node->next){
+                    node->right->next=node->next->left;
                 }
-                else{
-                    node1->next=q.front();
-                }
-                if(node1->left) q.push(node1->left);
-                if(node1->right) q.push(node1->right);
+
+                node=node->next;
             }
+            lvlStart=lvlStart->left;
         }
         return root;
     }
