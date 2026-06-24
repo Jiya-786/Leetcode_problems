a64ab6a0c9203881e822a43eb4d4537bdce52d27
Jiya_07 <merjajiya@gmail.com>
2026-06-24 10:52:28 +0530

    Time: 0 ms (100%), Space: 25.8 MB (21.31%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
@@ -11,32 +11,55 @@
  */
 
 // ITERATIVE INORDER TRAVERSAL
+// class Solution {
+// public:
+//     int getMinimumDifference(TreeNode* root) {
+//         stack<TreeNode*> st;
+//         vector<int> result;
+//         TreeNode* curr=root;
+        
+//         // st.push(root);     // not req since we are already pushing inside the while loop
+//         while(curr||!st.empty()){
+//             while(curr){
+//                 st.push(curr);
+//                 curr=curr->left;
+//             }
+//             curr=st.top();
+//             st.pop();
+//             result.push_back(curr->val);
+
+//             curr=curr->right;
+//         }
+
+//         int mini=INT_MAX;
+//         for(int i=1;i<result.size();i++){
+//             mini=min(mini,abs(result[i]-result[i-1]));
+//         }
+
+//         return mini;
+    
+//     }
+// };
+
+// RECURSIVE INORDER TRAVERSAL
 class Solution {
 public:
     int getMinimumDifference(TreeNode* root) {
-        stack<TreeNode*> st;
         vector<int> result;
-        TreeNode* curr=root;
-        
-        // st.push(root);     // not req since we are already pushing inside the while loop
-        while(curr||!st.empty()){
-            while(curr){
-                st.push(curr);
-                curr=curr->left;
-            }
-            curr=st.top();
-            st.pop();
-            result.push_back(curr->val);
-
-            curr=curr->right;
-        }
+        inorder(root,result);
 
         int mini=INT_MAX;
         for(int i=1;i<result.size();i++){
             mini=min(mini,abs(result[i]-result[i-1]));
         }
-
         return mini;
-    
+    }
+private: 
+    void inorder(TreeNode* node,vector<int>& result){
+        if(node==nullptr) return;
+
+        inorder(node->left,result);
+        result.push_back(node->val);
+        inorder(node->right,result);
     }
 };
