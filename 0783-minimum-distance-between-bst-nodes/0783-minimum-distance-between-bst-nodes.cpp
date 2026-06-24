c41ff9f458b7b0f3efd9eb1b5f08a725425eee80
Jiya_07 <merjajiya@gmail.com>
2026-06-24 10:51:31 +0530

    Time: 0 ms (100%), Space: 12.4 MB (58.11%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
@@ -1,29 +1,64 @@
-// ITERATIVE INORDER TRAVERSAL
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// // ITERATIVE INORDER TRAVERSAL
+// class Solution {
+// public:
+//     int minDiffInBST(TreeNode* root) {
+//         stack<TreeNode*> st;
+//         vector<int> result;
+//         TreeNode* curr=root;
+        
+//         // st.push(root);       // not req since we are already pushing inside the while loop
+//         while(curr||!st.empty()){
+//             while(curr){
+//                 st.push(curr);
+//                 curr=curr->left;
+//             }
+//             curr=st.top();
+//             st.pop();
+//             result.push_back(curr->val);
+
+//             curr=curr->right;
+//         }
+
+//         int mini=INT_MAX;
+//         for(int i=1;i<result.size();i++){
+//             mini=min(mini,abs(result[i]-result[i-1]));
+//         }
+
+//         return mini;
+//     }
+// };
+
+// RECURSIVE INORDER TRAVERSAL
 class Solution {
 public:
     int minDiffInBST(TreeNode* root) {
-        stack<TreeNode*> st;
         vector<int> result;
-        TreeNode* curr=root;
-        
-        // st.push(root);       // not req since we are already pushing inside the while loop
-        while(curr||!st.empty()){
-            while(curr){
-                st.push(curr);
-                curr=curr->left;
-            }
-            curr=st.top();
-            st.pop();
-            result.push_back(curr->val);
-
-            curr=curr->right;
-        }
+        inorder(root,result);
 
         int mini=INT_MAX;
         for(int i=1;i<result.size();i++){
             mini=min(mini,abs(result[i]-result[i-1]));
         }
-
         return mini;
+    }
+private: 
+    void inorder(TreeNode* node,vector<int>& result){
+        if(node==nullptr) return;
+
+        inorder(node->left,result);
+        result.push_back(node->val);
+        inorder(node->right,result);
     }
 };
