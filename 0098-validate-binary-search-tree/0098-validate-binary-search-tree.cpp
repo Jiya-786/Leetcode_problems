1e05e2ebf708bdc859d4d55c865ad89a5fa2476f
Jiya_07 <merjajiya@gmail.com>
2026-06-24 12:26:43 +0530

    Time: 0 ms (100%), Space: 21.8 MB (73.61%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,34 +1,50 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
 
- // BRUTE FORCE
+//  // BRUTE FORCE
+// class Solution {
+// public:
+//     bool isValidBST(TreeNode* root) {
+//         if(!root) return true;
+
+//         if(!(allLessThan(root->left,root->val) && allGreaterThan(root->right,root->val))) return false;
+
+//         return isValidBST(root->left) && isValidBST(root->right);
+//     }
+// private:
+//     bool allLessThan(TreeNode* node, int val){
+//         if(!node) return true;
+//         if(node->val >= val) return false;
+//         return allLessThan(node->left,val) && allLessThan(node->right,val);
+//     }
+//     bool allGreaterThan(TreeNode* node,int val){
+//         if(!node) return true;
+//         if(node->val<=val) return false;
+//         return allGreaterThan(node->left,val) && allGreaterThan(node->right,val);
+//     }
+// };
+
+// RECURSIVE RANGE VALIDATION
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        if(!root) return true;
-
-        if(!(allLessThan(root->left,root->val) && allGreaterThan(root->right,root->val))) return false;
-
-        return isValidBST(root->left) && isValidBST(root->right);
+        return validate(root,LONG_MIN,LONG_MAX);
     }
 private:
-    bool allLessThan(TreeNode* node, int val){
+    bool validate(TreeNode* node,long lower,long upper){
         if(!node) return true;
-        if(node->val >= val) return false;
-        return allLessThan(node->left,val) && allLessThan(node->right,val);
-    }
-    bool allGreaterThan(TreeNode* node,int val){
-        if(!node) return true;
-        if(node->val<=val) return false;
-        return allGreaterThan(node->left,val) && allGreaterThan(node->right,val);
+
+        if(node->val<=lower || node->val>=upper) return false;
+
+        return validate(node->left,lower,node->val) && validate(node->right,node->val,upper);
     }
 };
