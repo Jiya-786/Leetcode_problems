ca7d1a203d06f2574b1308a16f1f7bba04b531df
Jiya_07 <merjajiya@gmail.com>
2026-06-30 19:31:43 +0530

    Create 998-CheckCompletenessOfABinaryTree

---
+++ main/998-CheckCompletenessOfABinaryTree/998-CheckCompletenessOfABinaryTree.cpp
@@ -0,0 +1,36 @@
+// Last updated: 30/06/2026, 19:31:40
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isCompleteTree(TreeNode* root) {
+        queue<TreeNode*> q;
+        if(!root) return true;
+
+        q.push(root);
+
+        while(!q.empty() && q.front()!=nullptr){
+            int n=q.size();
+            while(n-- && q.front()!=nullptr){
+                TreeNode* node=q.front();
+                q.pop();
+
+                q.push(node->left);
+                q.push(node->right);
+            }
+        }
+        while(!q.empty() && q.front()==nullptr){
+            q.pop();
+        }
+        return (q.empty());
+    }
+};
