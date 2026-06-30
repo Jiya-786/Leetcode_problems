99913ca57eb1cff7391e94f8b8ee0fc67fb0f20f
Jiya_07 <merjajiya@gmail.com>
2026-06-30 18:52:28 +0530

    Clarify logic for maxPos check in canJump
    Added a comment to clarify the logic for returning false when the current index exceeds maxPos.

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -5,10 +5,10 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n=nums.size();
-        int maxPos=0;
+        int maxPos=0;      
         int pos=0;
         for(int i= 0;i<n;i++){
-            if(i>maxPos) return false;
+            if(i>maxPos) return false;       // becuase maxPos gives the max position we can reach at that given time, we can never go beyond maxPos, hence returning false
             maxPos=max(maxPos,nums[i]+i);
             if(maxPos>=n-1) return true;
         }
