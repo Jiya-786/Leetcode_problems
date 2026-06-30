5ed32ca08002e2ff6b0a084b320fa3f2e69bfcdc
Jiya_07 <merjajiya@gmail.com>
2026-06-30 19:34:10 +0530

    Create 236-LowestCommonAncestorOfABinaryTree

---
+++ main/236-LowestCommonAncestorOfABinaryTree/236-LowestCommonAncestorOfABinaryTree.cpp
@@ -0,0 +1,41 @@
+// Last updated: 30/06/2026, 19:34:09
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // Build parent map using BFS
+        unordered_map<TreeNode*, TreeNode*> parent;
+        queue<TreeNode*> bfsQueue;
+        parent[root] = nullptr;
+        bfsQueue.push(root);
+
+        // BFS until we've found both p and q's parents
+        while (parent.find(p) == parent.end() || parent.find(q) == parent.end()) {
+            TreeNode* node = bfsQueue.front();
+            bfsQueue.pop();
+            if (node->left) {
+                parent[node->left] = node;
+                bfsQueue.push(node->left);
+            }
+            if (node->right) {
+                parent[node->right] = node;
+                bfsQueue.push(node->right);
+            }
+        }
+
+        // Trace p's ancestors into a set
+        unordered_set<TreeNode*> ancestors;
+        TreeNode* current = p;
+        while (current != nullptr) {
+            ancestors.insert(current);
+            current = parent[current];
+        }
+
+        // Walk q upward until we find a common ancestor
+        current = q;
+        while (ancestors.find(current) == ancestors.end()) {
+            current = parent[current];
+        }
+
+        return current;
+    }
+};
