68a2ea9d23c9accbe9ffe804d7e609108f983438
Jiya_07 <merjajiya@gmail.com>
2026-07-01 13:13:03 +0530

    Time: 0 ms (100%), Space: 8.2 MB (16.28%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,26 +1,16 @@
-// DP approach
-// recursion+memoization
+// DP bottom-up approach
 class Solution {
 public:
-    int solve(int n, vector<int>& t) {
-        if (n == 0)
-            return 0;
-        if (n == 1)
-            return 1;
-
-        if (t[n] != -1)
-            return t[n];
-
-        return t[n] = solve(n - 1, t) + solve(n - 2, t);
-    }
-
     int fib(int n) {
-        vector<int> t(n + 1, -1);
-        if (n == 0)
-            return 0;
-        if (n == 1)
-            return 1;
+        if(n<=1) return n;
+        vector<int> t(n+1,-1);
 
-        return solve(n, t);
+        t[0]=0; t[1]=1;
+        for(int i=2;i<=n;i++){
+            t[i]=t[i-1]+t[i-2];
+        }
+
+        return t[n];
+        
     }
 };
