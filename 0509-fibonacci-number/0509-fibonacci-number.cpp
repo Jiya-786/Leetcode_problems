c938c7f33a996cce2d92fecb107752e4380b39e1
Jiya_07 <merjajiya@gmail.com>
2026-07-01 12:40:26 +0530

    Time: 0 ms (100%), Space: 8.1 MB (16.28%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -2,21 +2,25 @@
 // recursion+memoization
 class Solution {
 public:
+    int solve(int n, vector<int>& t) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
 
-    int solve(int n,vector<int>& t){
-        if(n==0) return 0;
-        if(n==1) return 1;
+        if (t[n] != -1)
+            return t[n];
 
-        if(t[n]!=-1) return t[n];
-
-        return t[n]=solve(n-1,t)+solve(n-2,t);
+        return t[n] = solve(n - 1, t) + solve(n - 2, t);
     }
 
     int fib(int n) {
-        vector<int> t(n+1,-1);
-        if(n==0) return 0;
-        if(n==1) return 1;
+        vector<int> t(n + 1, -1);
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
 
-        return solve(n,t);
+        return solve(n, t);
     }
 };
