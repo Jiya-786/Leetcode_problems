d966a949f20447e7db37c0ef04f97a2b4493c3ca
Jiya_07 <merjajiya@gmail.com>
2026-07-02 11:55:45 +0530

    Time: 0 ms (100%), Space: 7.6 MB (98.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,25 +1,19 @@
 // DP solution
-// recursion+memoization
-// time o(n) cuz Each subproblem from 1 to n is computed exactly once, doing O(1) work per subproblem.
-// space o(n)
+// bottom-up approach+space-optimized
 class Solution {
 public:
-
-    int solve(int n,vector<int>& t){
-        if(t[n]!=-1) return t[n];
-
-        return t[n]=solve(n-1,t)+solve(n-2,t);
-    }
-
     int climbStairs(int n) {
-        vector<int> t(n+1,-1);
-        t[0]=1;
-        // t[1]=1;
-        // t[2]=2;
-        // to prevent out of bounds eg when n=1, vector size=2 , indices 0,1 only will be available, so write:
-        if(n>=1) t[1]=1;
-        if(n>=2) t[2]=2;
+        // vector<int> t(n+1,-1);
+        if(n<=2) return n;
+        
+        int a=1; int b=2;
+        int c=0;
 
-        return solve(n,t);
+        for(int i=1;i<=n-2;i++){
+             c=a+b;
+             a=b;
+             b=c;
+        }
+        return c;
     }
 };
