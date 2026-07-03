c6698b28f9a8272da1e46f13df24eb31d2ca0ce2
Jiya_07 <merjajiya@gmail.com>
2026-07-03 09:15:53 +0530

    Change climbing stairs solution to brute force recursion
    Replaced dynamic programming approach with brute force recursion.

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,19 +1,10 @@
-// DP solution
-// bottom-up approach+space-optimized
+// BRUTE FORCE
+// simple recursion
 class Solution {
 public:
     int climbStairs(int n) {
-        // vector<int> t(n+1,-1);
         if(n<=2) return n;
-        
-        int a=1; int b=2;
-        int c=0;
 
-        for(int i=1;i<=n-2;i++){
-             c=a+b;
-             a=b;
-             b=c;
-        }
-        return c;
+        return climbStairs(n-1)+climbStairs(n-2);
     }
 };
