aa5010a10f22d1656fbeae001476de1d461e828a
Jiya_07 <merjajiya@gmail.com>
2026-07-03 12:20:45 +0530

    Time: 0 ms (100%), Space: 10.7 MB (22.76%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -6,6 +6,8 @@
 // t[i] will always me max psbl amt until ith house
 // When you decide to rob house i, you don't care whether house i-1 was skipped in some particular solution. You only care about the best legal profit that definitely doesn't involve house i-1, and that's exactly what t[i-2] guarantees.
 
+// space-optimized bottom-up
+
 class Solution {
 public:
     int rob(vector<int>& nums) {
@@ -14,16 +16,21 @@ public:
 
         if(n==1) return nums[0];
 
-        t[0]=0;
-        t[1]=nums[0];
+        int prevPrev=0;
+        int prev=nums[0];
 
         for(int i=2;i<=n;i++){
-            int skip=t[i-1];
-            int steal=nums[i-1]+t[i-2];
+            int skip=prev;
+            int steal=nums[i-1]+prevPrev;
 
             t[i]=max(skip,steal);
+
+            int temp=max(skip,steal);
+
+            prevPrev=prev;
+            prev=temp;
         }
-        return t[n];
+        return prev;
 
     }
 
