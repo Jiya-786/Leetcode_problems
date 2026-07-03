31566795555270af45152acff803bb80ae04c56f
Jiya_07 <merjajiya@gmail.com>
2026-07-03 16:12:07 +0530

    Time: 0 ms (100%), Space: 10.2 MB (73.57%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -4,37 +4,44 @@
 
 // always define state in bottom up approach
 // t[i] represents max value until ith house
+
+// now make this constant space
+// just like in house robber 1
 class Solution {
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> t(n+1,0);
+        // vector<int> t(n+1,0);
 
         if(n==1) return nums[0];
 
-        t[0]=0;
+        int prevPrev=0; int prev=0;
         // case-1 taking house-1 implies we can not take last house, implies only houses until n-1 should be under consideration
         // note index of t runs +1 than nums
         for(int i=1;i<=n-1;i++){
-            int skip=t[i-1];
-            int steal=nums[i-1]+((i-2)<=0 ? 0 : t[i-2] );
+            int skip=prev;
+            int steal=nums[i-1]+prevPrev;
 
-            t[i]=max(skip,steal);
+            int temp=max(skip,steal);
+            prevPrev=prev;
+            prev=temp;
         }
-        int res1=t[n-1];
+        int res1=prev;
 
-        t.clear();
-        t[0]=0;
-        t[1]=0;
+        // t.clear();
+        prev=0;
+        prevPrev=0;
         // case-2 not taking house1, implies we can take house2....last house(nth house)
         // note index of t runs +1 than nums
         for(int i=2;i<=n;i++){
-            int skip=t[i-1];
-            int steal=nums[i-1]+((i-2)<=0 ? 0 : t[i-2] );
+            int skip=prev;
+            int steal=nums[i-1]+prevPrev;
 
-            t[i]=max(skip,steal);
+            int temp=max(skip,steal);
+            prevPrev=prev;
+            prev=temp;
         }
-        int res2=t[n];
+        int res2=prev;
 
         return max(res1,res2);
 
