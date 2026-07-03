0da7d59aed555d6b0cc03de3d529354975f861b4
Jiya_07 <merjajiya@gmail.com>
2026-07-03 20:12:31 +0530

    Time: 0 ms (100%), Space: 7.9 MB (63.43%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,35 @@
+// classic BRUTE FORCE
+// time o(n*n^n-1): exponential
+// gives TLE for big n
+// notice repeating sub-problems
+// hence memoize
+// class Solution {
+// public:
+//     int integerBreak(int n) {
+//         if(n==1) return 1;
+
+//         int ans=0;
+//         for(int i=1;i<=n-1;i++){
+//             ans=max(ans,i*max(n-i,integerBreak(n-i)));
+//         }
+//         return ans;
+//     }
+// };
+
+// memoized soln
+class Solution {
+public:
+    vector<int> t=vector<int>(59,-1);    // or we can also resize inside the function on every call
+
+    int integerBreak(int n) {
+        if(n==1) return 1;
+
+        if(t[n]!=-1) return t[n];
+
+        int ans=0;
+        for(int i=1;i<=n-1;i++){
+            ans=max(ans,i*max(n-i,integerBreak(n-i)));
+        }
+        return t[n]=ans;
+    }
+};
