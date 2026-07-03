e6b8ac8c252bece0eac1d8d5599e0e7402fc7734
Jiya_07 <merjajiya@gmail.com>
2026-07-03 12:18:10 +0530

    Time: 0 ms (100%), Space: 10.5 MB (63.64%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,51 +1,30 @@
-// // BRUTE FORCE
-// // basic recursion
-// //gives TLE
-// // time o(2^n): exponential
-// // space o(n): due to call stack
-// class Solution {
-// public:
-//     int rob(vector<int>& nums) {
-//         return solve(nums,0);
-//     }
-// private:
-//     int solve(vector<int>& nums, int i){
-//         int n=nums.size();
-//         if(i>=n) return 0;
+// bottom-up approach
 
-//         int skip=solve(nums,i+1);
-//         int steal=nums[i]+solve(nums,i+2);
+// first thing always in this approach is defining the state
+// let t[i]= amount of money stolen until i'th house
+// so t[i] = max(stolen,skip)
+// t[i] will always me max psbl amt until ith house
+// When you decide to rob house i, you don't care whether house i-1 was skipped in some particular solution. You only care about the best legal profit that definitely doesn't involve house i-1, and that's exactly what t[i-2] guarantees.
 
-//         return max(skip,steal);
-//     }
-// };
-
-// BRUTE FORCE
-// basic recursion
-//gives TLE
-// time o(2^n): exponential
-// space o(n): due to call stack
-
-// hence memoize
-// time o(n)
-// space o(n)
 class Solution {
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> t(n+1,-1);
-        return solve(nums,0,t);
+        vector<int> t(n+1,0);
+
+        if(n==1) return nums[0];
+
+        t[0]=0;
+        t[1]=nums[0];
+
+        for(int i=2;i<=n;i++){
+            int skip=t[i-1];
+            int steal=nums[i-1]+t[i-2];
+
+            t[i]=max(skip,steal);
+        }
+        return t[n];
+
     }
-private:
-    int solve(vector<int>& nums, int i,vector<int>& t){
-        int n=nums.size();
-        if(i>=n) return 0;
 
-        if(t[i]!=-1) return t[i];
-
-        int skip=solve(nums,i+1,t);
-        int steal=nums[i]+solve(nums,i+2,t);
-
-        return t[i]=max(skip,steal);
-    }
 };
