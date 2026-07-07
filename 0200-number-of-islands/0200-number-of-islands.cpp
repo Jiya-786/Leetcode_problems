adc07bac5e30f94d695f48466f20a978f4535287
Jiya_07 <merjajiya@gmail.com>
2026-07-07 17:30:41 +0530

    Time: 28 ms (49.29%), Space: 18.3 MB (40.05%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,38 +1,92 @@
-// dfs soln 
+// // dfs soln 
+// class Solution {
+// public:
+//     int count=0;
+
+//     void dfs(vector<vector<char>>& grid,int i,int j){
+//             int n=grid.size();
+//             int m=grid[0].size();
+//             if(i<0||j<0||i>=n||j>=m||grid[i][j]=='0'){
+//                 return;
+//             }
+//             if(grid[i][j]=='2'){
+//                 return;
+//             }
+//             // count++;
+//             grid[i][j]='2';   // Since -1 is not a single character. It is - and 1. 
+
+//             dfs(grid,i+1,j);
+//             dfs(grid,i-1,j);
+//             dfs(grid,i,j+1);
+//             dfs(grid,i,j-1);
+// //          count++;
+//      }
+
+//     int numIslands(vector<vector<char>>& grid) {
+//         // int i=0; int j=0;
+//         int n=grid.size();
+//         int m=grid[0].size();
+
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if(grid[i][j]=='1'){
+//                     count++;
+//                     dfs(grid,i,j);                                                            
+//                     // return count;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+// bfs solution
 class Solution {
 public:
     int count=0;
 
-    void dfs(vector<vector<char>>& grid,int i,int j){
+    void bfs(vector<vector<char>>& grid,queue<pair<int,int>>& q){
             int n=grid.size();
             int m=grid[0].size();
-            if(i<0||j<0||i>=n||j>=m||grid[i][j]=='0'){
-                return;
-            }
-            if(grid[i][j]=='2'){
-                return;
-            }
-            // count++;
-            grid[i][j]='2';   // Since -1 is not a single character. It is - and 1. 
+            
+            while(!q.empty()){
+                auto it=q.front();
+                int i=it.first; int j=it.second;
+                q.pop();
 
-            dfs(grid,i+1,j);
-            dfs(grid,i-1,j);
-            dfs(grid,i,j+1);
-            dfs(grid,i,j-1);
-//          count++;
+                helper(grid,i+1,j,q);
+                helper(grid,i-1,j,q);
+                helper(grid,i,j-1,q);
+                helper(grid,i,j+1,q);
+
+            }
      }
+
+     void helper(vector<vector<char>>& grid,int i,int j,queue<pair<int,int>>& q){
+        int n=grid.size();
+        int m=grid[0].size();
+        if(i<0||j<0||i>=n||j>=m||grid[i][j]=='0'){
+            return;
+        }
+        if(grid[i][j]=='2'){
+            return;
+        }
+        q.push({i,j});
+        grid[i][j]='2';
+    }
 
     int numIslands(vector<vector<char>>& grid) {
         // int i=0; int j=0;
         int n=grid.size();
         int m=grid[0].size();
+        queue<pair<int,int>> q;
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]=='1'){
                     count++;
-                    dfs(grid,i,j);                                                            
-                    // return count;
+                    q.push({i,j});
+                    bfs(grid,q);
                 }
             }
         }
