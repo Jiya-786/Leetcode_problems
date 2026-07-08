26f2a37e1f28f022583c8c8a92b8f725116d534f
Jiya_07 <merjajiya@gmail.com>
2026-07-08 21:46:28 +0530

    Time: 106 ms (61.21%), Space: 131.4 MB (27.96%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,26 +1,68 @@
-// just dfs while maintaining max time
+// // just dfs while maintaining max time
+// class Solution {
+// public:
+
+//     // make note of this wrong dfs
+//     // The issue is that the recursion should return the maximum time from a node to any employee in its subtree, 
+//     //  rather than trying to maintain a single time variable passed down the recursion.
+
+//     // int dfs(vector<vector<int>>& adj,int u,vector<int>& informTime,int time){
+//     //     time=max(time,time+informTime[u]);
+
+//     //     for(int v:adj[u]){
+//     //         dfs(adj,v,informTime,time);
+//     //     }
+
+//     //     return time;
+//     // }
+
+//     void dfs(vector<vector<int>>& adj,int u,vector<int>& informTime,int currTime,int& maxTime){
+//         maxTime=max(maxTime,currTime);
+
+//         for(int v:adj[u]){
+//             dfs(adj,v,informTime,currTime+informTime[u],maxTime);
+//         }
+
+//     }
+
+//     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+//         // int time=0;
+
+//         vector<vector<int>> adj(n);      // need to decplare adj size since we're doing adj[manager[i]].push_back
+//                                          // (i), ow gives out of bounds 
+
+//         int m=manager.size();
+//         for(int i=0;i<m;i++){
+//             if(i==headID) continue;   // since if headID then manager[headID]=-1, gives seg-fault
+//             adj[manager[i]].push_back(i);
+//         }
+
+//         int maxTime=0;
+//         dfs(adj,headID,informTime,0,maxTime);    // should pass maxTime, can't pass 0, since passing by ref 
+//         return maxTime;
+
+        
+//     }
+// };
+
 class Solution {
 public:
-
-    // make note of this wrong dfs
-    // The issue is that the recursion should return the maximum time from a node to any employee in its subtree, 
-    //  rather than trying to maintain a single time variable passed down the recursion.
-
-    // int dfs(vector<vector<int>>& adj,int u,vector<int>& informTime,int time){
-    //     time=max(time,time+informTime[u]);
-
-    //     for(int v:adj[u]){
-    //         dfs(adj,v,informTime,time);
-    //     }
-
-    //     return time;
-    // }
-
-    void dfs(vector<vector<int>>& adj,int u,vector<int>& informTime,int currTime,int& maxTime){
-        maxTime=max(maxTime,currTime);
-
-        for(int v:adj[u]){
-            dfs(adj,v,informTime,currTime+informTime[u],maxTime);
+    void bfs(vector<vector<int>>& adj,int u,vector<int>& informTime,int currTime,int& maxTime,queue<pair<int,int>> q){
+        
+        while(!q.empty()) {
+            
+            auto temp = q.front();
+            int curr_emp  = temp.first;
+            int curr_time = temp.second;
+            
+            maxTime = max(maxTime, curr_time);
+            
+            q.pop();
+            
+            for(int &v : adj[curr_emp]) {
+                q.push({v, curr_time + informTime[curr_emp]}); //Time at which v recieves the information
+            }
+            
         }
 
     }
@@ -28,7 +70,7 @@ public:
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
         // int time=0;
 
-        vector<vector<int>> adj(n);      // need to decplare adj size since we're doing adj[manager[i]].push_back
+        vector<vector<int>> adj(n);      // need to declare adj size since we're doing adj[manager[i]].push_back
                                          // (i), ow gives out of bounds 
 
         int m=manager.size();
@@ -36,11 +78,13 @@ public:
             if(i==headID) continue;   // since if headID then manager[headID]=-1, gives seg-fault
             adj[manager[i]].push_back(i);
         }
-
+        queue<pair<int,int>> q;
+        q.push({headID,0});       // because apart from employee id we also need tot time until now and we need 
+                                  // to store it in q becuase unlike dfs recursion, we cannot have it stored in a 
+                                  // call          
         int maxTime=0;
-        dfs(adj,headID,informTime,0,maxTime);    // should pass maxTime, can't pass 0, since passing by ref 
+        bfs(adj,headID,informTime,0,maxTime,q);    // should pass maxTime, can't pass 0, since passing by ref 
         return maxTime;
 
-        
     }
 };
