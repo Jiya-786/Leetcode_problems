c4396bc1a0402c81086dcd49d3e0c6c2971dac60
Jiya_07 <merjajiya@gmail.com>
2026-07-12 14:38:25 +0530

    Time: 33 ms (8.49%), Space: 22.6 MB (19.51%) - LeetHub

--- 0690-employee-importance/0690-employee-importance.cpp
+++ 0690-employee-importance/0690-employee-importance.cpp
@@ -22,7 +22,7 @@ public:
         unordered_map<int,vector<int>> mp;
         for(auto it:employees){
             vector<int> temp;
-            temp.push_back(it->importance);
+            temp.push_back(it->importance);      // After almost completing the solution I realized we will also need the importance of every employee. Because I'm storing only subordinates and not pointer of every employee I had to first push employee importance into and then continue with the normal solution so that I also have access to the importance of the employee and I wouldn't have to redo the map input parameters. Instead of changing it from vector of int into employee pointer I did this work around.
             for(int x:it->subordinates){
                 temp.push_back(x);
             }
