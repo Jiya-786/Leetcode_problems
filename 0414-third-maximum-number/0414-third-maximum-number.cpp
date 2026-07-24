137e4b749483cc0a4da7a4bf1cfd787f931c639c
Jiya_07 <merjajiya@gmail.com>
2026-07-24 22:05:05 +0530

    Time: 0 ms (100%), Space: 12.9 MB (73.43%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -1,13 +1,42 @@
+// simple running counter
+// class Solution {
+// public:
+//     int thirdMax(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+
+//         nums.erase(unique(nums.begin(),nums.end()),nums.end());
+
+//         int n=nums.size();
+//         if(n<3) return nums[n-1];
+
+//         return nums[n-3];
+//     }
+// };
+
+// one pass soln- optimal
+// it mgiht be possible somethimes that the 3rd largets is int_min itself, so to be able to conclude it(third largest) exists or not we use llong min
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        long long first,second,third;
+        first=second=third=LLONG_MIN;
 
-        nums.erase(unique(nums.begin(),nums.end()),nums.end());
+        for(int n:nums){
+            if(n==first||n==second||n==third) continue;
 
-        int n=nums.size();
-        if(n<3) return nums[n-1];
-
-        return nums[n-3];
+            else if(n>first){
+                third=second;
+                second=first;
+                first=n;
+            }
+            else if(n>second){
+                third=second;
+                second=n;
+            }
+            else if(n>third){
+                third=n;
+            }
+        }
+        return third==LLONG_MIN ? first : third;
     }
 };
