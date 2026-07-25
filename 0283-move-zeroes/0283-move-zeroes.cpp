1d99a2a55a17d96683a662bcbd33313548b8ac4b
Jiya_07 <merjajiya@gmail.com>
2026-07-25 23:12:53 +0530

    Time: 0 ms (100%), Space: 23.8 MB (83.32%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,5 +1,9 @@
 // 2-pointer swapping method
 // insertPos will keep track of next pos of non-zero elt, and we keep swapping into it everytime there is a non-zero elt.
+
+// more optimized
+// lesser tot operations
+// removes redundant operations by ignoring trivial case when nums[i]!=0 and  i==insertPos.  
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
@@ -8,7 +12,10 @@ public:
 
         for(int i=0;i<n;i++){
             if(nums[i]!=0){
-                swap(nums[i],nums[insertPos]);
+                if(i!=insertPos){
+                    swap(nums[i],nums[insertPos]);
+                    // insertPos++;
+                }
                 insertPos++;
             }
         }
