50402fc7cb812f4043560e20ab64553381efc7fd
Jiya_07 <merjajiya@gmail.com>
2026-07-26 09:20:56 +0530

    Time: 0 ms (100%), Space: 22.6 MB (53.9%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,19 +1,40 @@
-// using hash-set
-// this is not inplace
+// // using hash-set
+// // this is not inplace
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         int n=nums.size();
+//         unordered_set<int> seen(nums.begin(),nums.end());
+//         vector<int> unique(seen.begin(),seen.end());
+//         sort(unique.begin(),unique.end());
+//         int k=unique.size();
+
+//         for(int i=0;i<k;i++){
+//             nums[i]=unique[i];
+//         }
+
+//         return k;
+
+//     }
+// };
+
+// two pointer swapping method
+// in-place
+//                i                        j   
+// 1    2    3    4    5    3    4    4    5
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int n=nums.size();
-        unordered_set<int> seen(nums.begin(),nums.end());
-        vector<int> unique(seen.begin(),seen.end());
-        sort(unique.begin(),unique.end());
-        int k=unique.size();
-
-        for(int i=0;i<k;i++){
-            nums[i]=unique[i];
+        int i=0; int j=1;
+        while(j<n){
+            if(nums[i]!=nums[j]){
+                nums[i+1]=nums[j];
+                i++;
+            }
+            j++;
+            
         }
-
-        return k;
-
+        return i+1;
     }
 };
