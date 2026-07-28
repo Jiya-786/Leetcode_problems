1d9a1b36e59946a1bb80fed7e2dd15e527ec5635
Jiya_07 <merjajiya@gmail.com>
2026-07-28 21:05:43 +0530

    Time: 0 ms (100%), Space: 23.7 MB (67.65%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,18 +1,87 @@
 // m1 direct hash-map method
+// class Solution {
+// public:
+//     vector<int> majorityElement(vector<int>& nums) {
+//         unordered_map<int,int> countMap;
+//         int n=nums.size();
+//         int threshold=n/3;
+//         vector<int> result;
+
+//         for(int num:nums){
+//             countMap[num]++;
+//         }
+//         for(auto& it:countMap){
+//             if(it.second>threshold) result.push_back(it.first);
+//         }
+//         return result;
+//     }
+// };
+
+// scan over the sorted array, measuring each run's length and comparing it against the threshold,
+// finds every majority element. This trades the O(n) space of a hash map for the O(n log n) time 
+// of sorting, and it modifies the input.
+// gives TLE though
+// class Solution {
+// public:
+//     vector<int> majorityElement(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int> result;
+//         sort(nums.begin(),nums.end());
+
+//         int i=1;
+//         int prev=nums[0];
+//         while(i<n){
+//             int count=0;
+//             while(i<n && nums[i]==prev){
+//                 count++;
+//                 i++;
+//             }
+//             if(count>n/3){
+//                 result.push_back(nums[i]);
+//             }
+//         }
+//         return result;
+//     }
+// };
+
+// boyer-moore voting algo
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int> countMap;
         int n=nums.size();
-        int threshold=n/3;
-        vector<int> result;
+        int cand1=0; int cand2=0;
+        int count1=0; int count2=0;
 
         for(int num:nums){
-            countMap[num]++;
+            if(num==cand1) count1++;
+            else if(num==cand2) count2++;
+            else if(count1==0){
+                cand1=num;
+                count1=1;
+            }
+            else if(count2==0){
+                cand2=num;
+                count2=1;
+            }
+            else{
+                count1--;
+                count2--;
+            }
         }
-        for(auto& it:countMap){
-            if(it.second>threshold) result.push_back(it.first);
+        // WHY verification is req here-
+
+        // For n/2, the majority element has more votes than everyone else combined, so it cannot lose.
+        // For n/3, it only has more votes than one-third, so it can lose during cancellation; therefore we must recount.
+
+        count1=0; count2=0;
+        for(int num:nums){
+            if(num==cand1) count1++;
+            else if(num==cand2) count2++;
         }
+
+        vector<int> result;
+        if(count1>n/3) result.push_back(cand1);
+        if(count2>n/3) result.push_back(cand2);
         return result;
     }
 };
