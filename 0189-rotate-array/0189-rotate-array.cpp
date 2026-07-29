ec0b75558891dc6a3cb56896c803fcc469eb9b24
Jiya_07 <merjajiya@gmail.com>
2026-07-29 16:03:03 +0530

    Time: 0 ms (100%), Space: 29.7 MB (46.79%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -17,19 +17,41 @@
 // };
 
 // extra array
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//         int n=nums.size();
+//         k=k%n;
+//         vector<int> temp(n);
+
+//         for(int i=0;i<n;i++){
+//             temp[(i+k)%n]=nums[i];
+//         }
+
+//         for(int i=0;i<n;i++){
+//             nums[i]=temp[i];
+//         }
+//     }
+// };
+
+// reversing array
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
         k=k%n;
-        vector<int> temp(n);
 
-        for(int i=0;i<n;i++){
-            temp[(i+k)%n]=nums[i];
-        }
-
-        for(int i=0;i<n;i++){
-            nums[i]=temp[i];
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
+    }
+    void reverse(vector<int>& nums,int left,int right){
+        while(left<right){
+            int temp=nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+            left++;
+            right--;
         }
     }
 };
