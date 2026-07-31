5609ec65b53976a1480c135f31be20bbb61674bf
Jiya_07 <merjajiya@gmail.com>
2026-07-31 12:59:44 +0530

    Time: 0 ms (100%), Space: 111.3 MB (74.05%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -1,19 +1,31 @@
+// brute force
+// chacking number of consecutive zeroes
+// for k consecutive zeroes we get k*k+1/2 zeroes
+// becuase for k numbers we have k+1 boundaries, and for a subarray we choose any 2, so k+1c2, so k+1*k/2
+// here max n=10^5 so at max there can be n consecutive zeroes
+// so at max ~10^10/2 ~ 5*10^9 can be the ans
+// 5*10^9 will not fit in int
+// hence we need to use long long
 class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
-        long long count = 0;
-        int consecutiveZeros = 0;
+        int n=nums.size();
+        long long count=0;
 
-        for (int num : nums) {
-            if (num == 0) {
-                consecutiveZeros++;
-            } else {
-                consecutiveZeros = 0;
+        int i=0;
+        while(i<n){
+            if(nums[i]!=0){
+                i++;
+            } 
+            else{
+                long long x=0;
+                while(i<n && nums[i]==0){
+                    x++;
+                    i++;
+                }
+                count+=((x)*(x+1)/2);
             }
-            // Each new zero in a run of length k adds k new subarrays
-            count += consecutiveZeros;
         }
-
         return count;
     }
 };
