5ec0eb65c4a1cda66c7c4f099fbe2eeb591af315
Jiya_07 <merjajiya@gmail.com>
2026-07-31 13:24:09 +0530

    Time: 0 ms (100%), Space: 115.6 MB (92.33%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,22 +1,47 @@
+// brute force
+// give TLE obvio
+// class Solution {
+// public:
+//     bool increasingTriplet(vector<int>& nums) {
+//         int n=nums.size();
+//         bool ans;
+
+//         for(int i=0;i<n;i++){
+//             for(int j=i;j<n;j++){
+//                 if(nums[i]<nums[j]){
+//                     for(int k=j;k<n;k++){
+//                         if(nums[j]<nums[k]){
+//                             return true;
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+
+// optimized 2-pointer approach
+// watch MIKS vid for greatest level of clarity and reasoning
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        int first = INT_MAX;
-        int second = INT_MAX;
+        int n=nums.size();
+        int first=INT_MAX;
+        int second=INT_MAX;
 
-        for (int num : nums) {
-            if (num <= first) {
-                // Smallest value so far
-                first = num;
-            } else if (num <= second) {
-                // Second smallest value (greater than some earlier first)
-                second = num;
-            } else {
-                // Found a value greater than both first and second
+        for(int i=0;i<n;i++){
+            if(nums[i]<=first){
+                first=nums[i];
+            }
+            else if(nums[i]<=second){
+                second=nums[i];
+            }
+            else{
                 return true;
             }
         }
-
         return false;
     }
 };
