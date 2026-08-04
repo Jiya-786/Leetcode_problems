eb143afbbec435cc36a6902d0390b4d87ca79ca9
Jiya_07 <merjajiya@gmail.com>
2026-08-04 11:28:42 +0530

    Time: 0 ms (100%), Space: 27 MB (98.71%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,16 +1,34 @@
+// bute force
 //using extra array
+// class Solution {
+// public:
+//     void reverseString(vector<char>& s) {
+//         vector<char> ans;
+//         int n=s.size();
+        
+//         for(int i=n-1;i>=0;i--){
+//             ans.push_back(s[i]);
+//         }
+
+//         for(int i=0;i<n;i++){
+//             s[i]=ans[i];
+//         }
+//     }
+// };
+
+// in-place using two pointers
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        vector<char> ans;
         int n=s.size();
+        int left=0;
+        int right=n-1;
         
-        for(int i=n-1;i>=0;i--){
-            ans.push_back(s[i]);
-        }
-
-        for(int i=0;i<n;i++){
-            s[i]=ans[i];
+        while(left<right){
+            swap(s[left],s[right]);
+            left++;
+            right--;
         }
     }
 };
+
