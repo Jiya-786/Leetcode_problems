357a57561b85c2d84e7a38254adc1474d1ab35ca
Jiya_07 <merjajiya@gmail.com>
2026-08-04 18:57:58 +0530

    Time: 0 ms (100%), Space: 8.3 MB (9.86%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,15 @@
+// brute force
+// checking all rotations
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length()!=goal.length()) return false;
+
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            string rotated=s.substr(i)+s.substr(0,i);
+            if(rotated==goal) return true;
+        }
+        return false;
+    }
+};
