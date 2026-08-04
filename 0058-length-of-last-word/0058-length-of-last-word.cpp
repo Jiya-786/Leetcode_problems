f6323c2e07b870b77026b2bdb37d5b9cd7f39d3d
Jiya_07 <merjajiya@gmail.com>
2026-08-04 12:02:45 +0530

    Time: 0 ms (100%), Space: 8.9 MB (69.32%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -2,14 +2,33 @@
 // on time
 // on space
 // since the split allocates a list of all words, which in the worst case holds every character.
+// class Solution {
+// public:
+//     int lengthOfLastWord(string s) {
+//         istringstream stream(s);
+//         string word,last;
+//         while(stream>>word){
+//             last=word;
+//         }
+//         return last.length();
+//     }
+// };
+
+// scan from end
+// optimal
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        istringstream stream(s);
-        string word,last;
-        while(stream>>word){
-            last=word;
+        int n=s.length();
+
+        int i=n-1;
+        while(i>=0 && s[i]==' ') i--;
+
+        int ans=0;
+        while (i>=0 && s[i]!=' '){
+            ans++;
+            i--;
         }
-        return last.length();
+        return ans;
     }
 };
