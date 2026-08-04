5cb57b8d04ff8c41f057608283b8ae1f419025e3
Jiya_07 <merjajiya@gmail.com>
2026-08-04 19:05:48 +0530

    Time: 0 ms (100%), Space: 8.2 MB (36.98%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,15 +1,26 @@
 // brute force
 // checking all rotations
+// class Solution {
+// public:
+//     bool rotateString(string s, string goal) {
+//         if(s.length()!=goal.length()) return false;
+
+//         int n=s.length();
+//         for(int i=0;i<n;i++){
+//             string rotated=s.substr(i)+s.substr(0,i);
+//             if(rotated==goal) return true;
+//         }
+//         return false;
+//     }
+// };
+
+// concatenate and search
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if(s.length()!=goal.length()) return false;
+        if(s.size()!=goal.size()) return false;
 
-        int n=s.length();
-        for(int i=0;i<n;i++){
-            string rotated=s.substr(i)+s.substr(0,i);
-            if(rotated==goal) return true;
-        }
-        return false;
+        string doubled=s+s;
+        return doubled.find(goal)!=string::npos;
     }
 };
