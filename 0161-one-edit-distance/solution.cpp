a9b164f51f95cad08697563115e6e5fe7f28cc7b
Jiya_07 <merjajiya@gmail.com>
2026-08-08 16:45:46 +0530

    Add isOneEditDistance function to Solution class
    Implement logic to check if two strings are one edit distance apart.

--- 0161-one-edit-distance/solution.cpp
+++ 0161-one-edit-distance/solution.cpp
@@ -1,1 +1,30 @@
+// if we can delete and make both strings equal, that means we can also insert in the otehr string and make both equal
+// hence to avoid that confusion and compress insert and delete into one case, let's always make the longer string as s
+// this is oN time
+// on space becase substr creates new strings and in worst case it can be proportional to n
+class Solution {
+public:
+    bool isOneEditDistance(string s, string t) {
+        int n=s.size();
+        int m=t.size();
 
+        if(abs(n-m)>1) return false;
+
+        else{
+            if(n-m<1) swap(s,t);
+            // so now s is the longer string
+
+            for(int i=0;i<t.length();i++){
+                if(s[i]!=t[i]){
+                    if(s.size()==t.size()){
+                        return s.substr(i+1)==t.substr(i+1);
+                    }
+                    else{
+                        return s.substr(i+1)==t.substr(i);
+                    }
+                }
+            }
+            return s.length()==t.length()+1;
+        }
+    }
+};
