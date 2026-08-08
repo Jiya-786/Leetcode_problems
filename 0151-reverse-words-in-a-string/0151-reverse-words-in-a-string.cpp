851d3ac4179fd9e1eb97737b606ef7771fcd98a3
Jiya_07 <merjajiya@gmail.com>
2026-08-08 19:03:24 +0530

    Time: 0 ms (100%), Space: 9.9 MB (88.25%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -2,26 +2,51 @@
 // on space soln
 // splitting using istringstream
 // istringstream inherently handles spaces in input
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         stack<string> st;
+//         istringstream stream(s);
+//         string word;
+
+//         while(stream>>word){
+//             st.push(word);
+//         }
+
+//         string ans="";
+
+//         while(!st.empty()){
+//             ans+=st.top();
+//             st.pop();
+
+//             if(st.size()>=1) ans+=" ";
+//         }
+
+//         return ans;
+//     }
+// };
+
+// now to make it o1 space we need to use 2 pointers and do right to left traversal
+// this doesnt inherently handle extra spaces like istringstream
+// we need extra care for that case
 class Solution {
 public:
     string reverseWords(string s) {
-        stack<string> st;
-        istringstream stream(s);
-        string word;
-
-        while(stream>>word){
-            st.push(word);
-        }
-
         string ans="";
+        int n=s.size()-1;
+        
+        int i=n;
+        while(i>=0){
+            while(i>=0 && s[i]==' ') i--;
+            if(i<0) break;
 
-        while(!st.empty()){
-            ans+=st.top();
-            st.pop();
+            int end=i;
+            while(i>=0 && s[i]!=' ') i--;
 
-            if(st.size()>=1) ans+=" ";
+            if(!ans.empty()) ans+=' ';
+            ans+=s.substr(i+1,end-i);   // rember the 2nd paramet is length, not end_index. 
+
         }
-
         return ans;
     }
 };
