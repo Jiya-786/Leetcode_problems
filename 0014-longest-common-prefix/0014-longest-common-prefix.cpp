65f6bb7d50069e3d92852a90c646208c6c9d4c68
Jiya_07 <merjajiya@gmail.com>
2026-08-08 10:50:55 +0530

    Time: 0 ms (100%), Space: 12.1 MB (13.22%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -2,20 +2,42 @@
 // vertical scanning
 // brute force
 // check every possible prefix
+// class Solution {
+// public:
+//     string longestCommonPrefix(vector<string>& strs) {
+//         if(strs.empty()) return "";
+
+//         for(int i=0;i<strs[0].length();i++){
+//             char c=strs[0][i];
+
+//             for(int j=1;j<strs.size();j++){
+//                 if(i==strs[j].length()||c!=strs[j][i]){
+//                     return strs[0].substr(0,i);
+//                 }
+//             }
+//         }
+//         return strs[0];
+//     }
+// };
+
+// sort lexicographically
+// like in a dictionary
+// the first and last string common prefix after sorting
+// that will be the longest common prefix
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        if(strs.empty()) return "";
+        if (strs.empty()) return "";
 
-        for(int i=0;i<strs[0].length();i++){
-            char c=strs[0][i];
+        sort(strs.begin(),strs.end());
 
-            for(int j=1;j<strs.size();j++){
-                if(i==strs[j].length()||c!=strs[j][i]){
-                    return strs[0].substr(0,i);
-                }
-            }
+        string first=strs[0];
+        string last=strs[strs.size()-1];
+
+        int i=0;
+        while(i<first.length() && i<last.length() && first[i]==last[i]){
+            i++;
         }
-        return strs[0];
+        return strs[0].substr(0,i);
     }
 };
